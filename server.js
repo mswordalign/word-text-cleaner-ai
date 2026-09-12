b220f686553d193504999c60eaad7b32e749eabd
guruvujk <guruvu@gmail.com>
2026-09-12 10:26:12 +0530

    add real render url, static file serving, and icons

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.send('Word Text Cleaner AI backend is running.');
